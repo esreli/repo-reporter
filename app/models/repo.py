556695f6deed2e99338f76838da2287f562a2262
Eli Gregory <egregory@esri.com>
2019-11-20 11:14:40 -0800

    flattens yaml

--- app/models/repo.py
+++ app/models/repo.py
@@ -2,16 +2,7 @@ class Repo(object):
 
     @staticmethod
     def from_yaml(yaml_stream):
-        repos = []
-        for family in yaml_stream["families"]:
-            family_name = family["name"]
-            platforms = family["platforms"]
-            for platform in platforms:
-                platform_name = platform["platform"]
-                platform_repo = platform["repo"]
-                repo = Repo(family_name, platform_name, platform_repo)
-                repos.append(repo)
-        return repos
+        return [Repo(repo["name"], repo["platform"], repo["repo"]) for repo in yaml_stream["repos"]]
 
     def __init__(self, family_name, platform, repo):
         self.family_name = family_name
