0e1d2bea60f3663a3a863957715ec168903d4a56
Eli Gregory <egregory@esri.com>
2019-11-25 10:39:20 -0800

    introduces path

--- app/models/insight.py
+++ app/models/insight.py
@@ -1,5 +1,6 @@
 from app import db
 from .referrer import Referrer
+from .path import Path
 
 class Insight(object):
 
@@ -7,8 +8,10 @@ class Insight(object):
         self.repo = repo
         self.paths = []
 
-        # DB Aggregate
-        agr = [{'$match': {'repo': self.repo.repo, 'timestamp': {'$gte': start, '$lte': end}}}, {'$group': {'_id': 1, 'count': {'$sum': '$count'}, 'uniques': {'$sum': '$uniques'}}}]
+        match = {'$match': {'repo': self.repo.repo, 'timestamp': {'$gte': start, '$lte': end}}}
+
+        # Views & Clones
+        agr = [match, {'$group': {'_id': 1, 'count': {'$sum': '$count'}, 'uniques': {'$sum': '$uniques'}}}]
 
         # Views
         view = list(db.view.aggregate(agr))
@@ -20,13 +23,15 @@ class Insight(object):
         self.clone_count = Insight.__get_value(clone, 'count') or 0
         self.clone_uniques = Insight.__get_value(clone, 'uniques') or 0
 
-        # Paths
-        # TODO
-
         # Referrers
-        agr = [{'$match': {'repo': self.repo.repo, 'timestamp': {'$gte': start, '$lte': end}}}, {'$group': {'_id': '$referrer', 'count': {'$sum': '$count'}, 'uniques': {'$sum': '$uniques'}}}, {'$sort': {'count': -1}}]
+        agr = [match, {'$group': {'_id': '$referrer', 'count': {'$sum': '$count'}, 'uniques': {'$sum': '$uniques'}}}, {'$sort': {'count': -1}}]
         referrers = list(db.referrer.aggregate(agr))
         self.referrers = [ Referrer(ref["_id"], ref["count"], ref["uniques"]) for ref in referrers ]
+
+        # Paths
+        agr = [match, {'$group': {'_id': '$path', 'title': {'$first': '$title'}, 'count': {'$sum': '$count'}, 'uniques': {'$sum': '$uniques'}}}, {'$sort': {'count': -1}}, {'$limit': 10}]
+        paths = list(db.path.aggregate(agr))
+        self.paths = [ Path(path["_id"], path["title"], path["count"], path["uniques"]) for path in paths]
 
     def __repr__(self):
         return 'Insight({0}- v: {1}, vu: {2}, c: {3}, cu: {4})'.format(self.repo.repo, self.view_count, self.view_uniques, self.clone_count, self.clone_uniques)
---
+++ app/models/path.py
@@ -0,0 +1,14 @@
+
+class Path(object):
+
+    def __init__(self, path, title, count, uniques):
+        self.path = path
+        self.title = title
+        self.count = round(count+0.5)
+        self.uniques = round(uniques+0.5)
+
+    def __repr__(self):
+        return "Path({0} - c: {1}, u: {2})".format(self.path, self.count, self.uniques)
+
+    def url(self, base="https://github.com"):
+        return "{0}{1}".format(base, self.path)
